e78a2b997285e00c4ef69923afbfb963ac546be7
devyanit05 <thokaldevyani123@gmail.com>
2022-10-13 12:55:10 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles.css'
+
 
 function App() {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [res, setRes] = useState(0);
+
+  function add(e) {
+    e.preventDefault();
+    setRes(parseInt(num1) + parseInt(num2));
+
+  }
+
+  function substract(e) {
+    e.preventDefault();
+    setRes(parseInt(num1) - parseInt(num2));
+  }
+
+  function multiply(e) {
+    e.preventDefault();
+    setRes(parseInt(num1) * parseInt(num2));
+  }
+
+  function divide(e) {
+    e.preventDefault();
+    setRes(parseInt(num1) + parseInt(num2));
+  }
+
+  const handleChange1 = (e) => {
+    setNum1(e.target.value);
+  }
+
+  const handleChange2 = (e) => {
+    setNum2(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className='container'>
+      <h1>Calculator</h1>
+      <article>
+        <label>
+          Num 1:
+          <input type="Number" name="num1" value={num1} onChange={handleChange1} />
+        </label>
+        <label>
+          Num 2:
+          <input type="Number" name="num2" value={num2} onChange={handleChange2} />
+        </label>
+      </article>
+      <div>
+        <button onClick={add}>+</button>
+        <button onClick={substract}>-</button>
+        <button onClick={multiply}>x</button>
+        <button onClick={divide}>/</button>
+      </div>
+      <h2>Ans: {res}</h2>
+    </form>
   );
 }
 
